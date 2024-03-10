3846e2d242989f8c3d68fa78c20ebe5b490cf8e2
Thai Ly <thai.ly@masterisehomes.com>
2024-03-10 22:45:13 +0700

    config theme switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
+import { useColorScheme } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
 
-export default function App() {
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
   return (
-	  <Container maxWidth="sm">
-		  <Box sx={{ my: 4 }}>
-			  <Typography variant="body2" component="h1" color="text.secondary" sx={{ mb: 2 }}>
-				  Material UI Vite.js example
-			  </Typography>
-		  </Box>
-	  </Container>
+    <Button onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}>
+	    {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
   )
+}
+
+export default function App() {
+  return (<Container maxWidth="sm">
+    <Box sx={{ my: 4 }}>
+      <ModeToggle></ModeToggle>
+      <Typography variant="body2" component="h1" color="text.secondary" sx={{ mb: 2 }}>
+				Material UI Vite.js example
+      </Typography>
+    </Box>
+  </Container>)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
+import CssBaseline from '@mui/material/CssBaseline'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
-import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@mui/material/styles'
 import App from './App'
 import theme from './theme'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
-root.render(<React.StrictMode>
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline/>
-    <App/>
-  </ThemeProvider>
-</React.StrictMode>)
+root.render(
+  <React.StrictMode>
+	  <CssVarsProvider theme={theme}>
+		  {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+		  <CssBaseline/>
+		  <App/>
+	  </CssVarsProvider>
+  </React.StrictMode>
+)
