979fe44531213d09a9811de4192d152242e78d0a
thaily <lyquangthai1993@gmail.com>
2024-03-16 21:57:18 +0700

    watch video FE 25

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,31 +1,33 @@
-import { LibraryAdd } from '@mui/icons-material'
+import { Close, LibraryAdd, Search } from '@mui/icons-material'
 import Apps from '@mui/icons-material/Apps'
-import { SvgIcon, Typography } from '@mui/material'
+import { InputAdornment, SvgIcon, TextField, Typography } from '@mui/material'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import { useState } from 'react'
 import { ReactComponent as Logo } from '~/assets/slack.svg'
 import WorkSpace from '~/components/AppBar/Menus/WorkSpace'
 import ModeSelect from '~/components/ModeSelect'
 
 function AppBar() {
+  const [searchValue, setSearchValue] = useState('')
   return (
-	  <Box sx={{
-		  with: '100%', height: (theme) => `${theme.trelloCutom.appBarHeight}`, display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: 2, padding: 2,
-		  overflowX: 'auto',
-		  overflowY: 'hidden',
-		  bgcolor: theme => theme.palette.mode === 'dark' ? '#2c3e50' : '#1565c0'
-	  }}>
-		  <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-			  <Apps sx={{ color: 'white' }}/>
-			  <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-				  <SvgIcon component={Logo} inheritViewBox fontSize="small" sx={{ color: 'white' }}/>
-				  <Typography variant="span" sx={{ fontSize: '1.2', color: 'white' }}>Trello</Typography>
-			  </Box>
-			  <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-				  <WorkSpace/>
-				  <WorkSpace/>
-				  <WorkSpace/>
-				  <Button
+    <Box sx={{
+      with: '100%', height: (theme) => `${theme.trelloCutom.appBarHeight}`, display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: 2, padding: 2,
+      overflowX: 'auto',
+      overflowY: 'hidden',
+      bgcolor: theme => theme.palette.mode === 'dark' ? '#2c3e50' : '#1565c0'
+    }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <Apps sx={{ color: 'white' }}/>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon component={Logo} inheritViewBox fontSize="small" sx={{ color: 'white' }}/>
+          <Typography variant="span" sx={{ fontSize: '1.2', color: 'white' }}>Trello</Typography>
+        </Box>
+        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <WorkSpace/>
+          <WorkSpace/>
+          <WorkSpace/>
+          <Button
             variant="outlined"
             startIcon={<LibraryAdd/>}
             sx={{
@@ -33,13 +35,61 @@ function AppBar() {
                 border: 'none'
               }
             }}
-				  >Create</Button>
-			  </Box>
-		  </Box>
-		  <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-			  <ModeSelect/>
-		  </Box>
-	  </Box>
+          >Create</Button>
+        </Box>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <TextField
+          id="outline-search"
+          label="Search..."
+          type="text"
+          size={'small'}
+          value={searchValue}
+          onChange={e => setSearchValue(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search sx={{ color: 'white' }}/>
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <Close
+                fontSize={'small'}
+                sx={{
+                  color: 'white',
+                  cursor: 'pointer'
+                }}
+                onClick={() => setSearchValue('')}
+              />)
+          }}
+          sx={{
+            minWidth: 120,
+            maxWidth: 180,
+            '& label': {
+              color: 'white'
+            },
+            '& input': {
+              color: 'white'
+            },
+            '& label.Mui-focused': {
+              color: 'white'
+            },
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: 'white'
+              },
+              '&:hover fieldset': {
+                borderColor: 'white'
+              },
+              '&.Mui-focuced fieldset': {
+                borderColor: 'white'
+              }
+            }
+          }}
+        />
+        <ModeSelect/>
+      </Box>
+    </Box>
   )
 }
 
--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -16,41 +16,50 @@ function Index() {
   }
 
   return (
-	  <FormControl size="small" sx={{ minWidth: 120 }}>
-		  <InputLabel id="demo-select-dark-light-mode">Mode</InputLabel>
-		  <Select
+    <FormControl size="small" sx={{ minWidth: 120 }}>
+      <InputLabel id="demo-select-dark-light-mode"
+        sx={{
+          color: 'white', '&.Mui-focused': {
+            color: 'white'
+          }
+        }}>Mode< /InputLabel>
+      <Select
         labelId="demo-select-dark-light-mode"
         id="demo-select-dark-light-mode"
         value={mode}
         label="Mode"
         onChange={handleChange}
         sx={{
-          color: 'white'
+          color: 'white',
+          '.MuiOutlinedInput-notchedOutline': { borderColor: 'white' },
+          '&:hover .MuiOutlinedInput-notchedOutline': { borderColor: 'white' },
+          '&.Mui-focused .MuiOutlinedInput-notchedOutline': { borderColor: 'white' },
+          '.MuiSvgIcon-root': { color: 'white' }
         }}
-		  >
-			  <MenuItem value={'light'}>
-				  <Box sx={{
-					  display: 'flex', alignItems: 'center', gap: 1
-				  }}>
-					  <LightModeIcon fontSize="small"/> Light
-				  </Box>
-			  </MenuItem>
-			  <MenuItem value={'dark'}>
-				  <Box sx={{
-					  display: 'flex', alignItems: 'center', gap: 1
-				  }}>
-					  <DarkModeIcon fontSize="small"/> Dark
-				  </Box>
-			  </MenuItem>
-			  <MenuItem value={'system'}>
-				  <Box sx={{
-					  display: 'flex', alignItems: 'center', gap: 1
-				  }}>
-					  <SettingsBrightnessIcon fontSize="small"/> System
-				  </Box>
-			  </MenuItem>
-		  </Select>
-	  </FormControl>)
+      >
+        <MenuItem value={'light'}>
+          <Box sx={{
+            display: 'flex', alignItems: 'center', gap: 1
+          }}>
+            <LightModeIcon fontSize="small"/> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value={'dark'}>
+          <Box sx={{
+            display: 'flex', alignItems: 'center', gap: 1
+          }}>
+            <DarkModeIcon fontSize="small"/> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value={'system'}>
+          <Box sx={{
+            display: 'flex', alignItems: 'center', gap: 1
+          }}>
+            <SettingsBrightnessIcon fontSize="small"/> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>)
 }
 
 export default Index
