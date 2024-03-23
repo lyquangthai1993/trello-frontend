a9ec0a8633bf8c1dc7ea768056cf98b0f6820771
thaily <lyquangthai1993@gmail.com>
2024-03-23 21:02:51 +0700

    watch video BE 68

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -12,71 +12,71 @@ import ListCards from '~/pages/Boards/BoardContent/ListColumns/Column/ListCards/
 import { mapOrder } from '~/utils/sorts'
 
 function Column({ column }) {
-	const {
-		attributes,
-		listeners,
-		setNodeRef,
-		transform,
-		transition,
-		isDragging
-	} = useSortable({
-		id: column._id,
-		data: { ...column }
-	})
-	const dndKitColumnStyles = {
-		// touchAction: 'none',
-		// nếu sử dụng transform: CSS.Transform sẽ lỗi dạng stretch column
-		// github issue: https://github.com/clauderic/dnd-kit/issues/117
-		transform: CSS.Translate.toString(transform),
-		transition,
-		height: '100%',
-		opacity: isDragging ? 0.5 : undefined
-		
-	}
-	
-	const [anchorEl, setAnchorEl] = useState(null)
-	const open = Boolean(anchorEl)
-	
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget)
-	}
-	const handleClose = () => {
-		setAnchorEl(null)
-	}
-	
-	const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, '_id')
-	
-	const [openNewCardForm, setOpenNewCardForm] = useState(false)
-	const [newCardTitle, setNewCardTitle] = useState('')
-	const toggleNewCardForm = () => setOpenNewCardForm(!openNewCardForm)
-	const addNewCard = () => {
-		// call api
-		// add new column to columns
-		// close form
-		if (newCardTitle.trim() === '') {
-			toast.error('Column title is required', {
-				position: 'top-right'
-			})
-			return
-		}
-		
-		toggleNewCardForm()
-		setOpenNewCardForm('')
-	}
-	
-	return (
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging
+  } = useSortable({
+    id: column._id,
+    data: { ...column }
+  })
+  const dndKitColumnStyles = {
+    // touchAction: 'none',
+    // nếu sử dụng transform: CSS.Transform sẽ lỗi dạng stretch column
+    // github issue: https://github.com/clauderic/dnd-kit/issues/117
+    transform: CSS.Translate.toString(transform),
+    transition,
+    height: '100%',
+    opacity: isDragging ? 0.5 : undefined
+
+  }
+
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, '_id')
+
+  const [openNewCardForm, setOpenNewCardForm] = useState(false)
+  const [newCardTitle, setNewCardTitle] = useState('')
+  const toggleNewCardForm = () => setOpenNewCardForm(!openNewCardForm)
+  const addNewCard = () => {
+    // call api
+    // add new column to columns
+    // close form
+    if (newCardTitle.trim() === '') {
+      toast.error('Column title is required', {
+        position: 'top-right'
+      })
+      return
+    }
+
+    toggleNewCardForm()
+    setOpenNewCardForm('')
+  }
+
+  return (
 	  <div ref={setNodeRef} style={dndKitColumnStyles} {...attributes} >
 		  <Box
-			sx={{
-				minWidth: '300px',
-				maxWidth: '300px',
-				bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
-				ml: 2,
-				borderRadius: '6px',
-				height: 'fit-content',
-				maxHeight: (theme) => `calc(${theme.trelloCutom.boardContentHeight} - ${theme.spacing(5)})`
-			}}
-			{...listeners}
+        sx={{
+          minWidth: '300px',
+          maxWidth: '300px',
+          bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
+          ml: 2,
+          borderRadius: '6px',
+          height: 'fit-content',
+          maxHeight: (theme) => `calc(${theme.trelloCutom.boardContentHeight} - ${theme.spacing(5)})`
+        }}
+        {...listeners}
 		  >
 			  <Box sx={{
 				  height: (theme) => theme.trelloCutom.columnHeaderHeight,
@@ -86,40 +86,40 @@ function Column({ column }) {
 				  justifyContent: 'space-between'
 			  }}>
 				  <Typography
-					variant={'h6'}
-					sx={{
-						fontSize: '1rem',
-						fontWeight: 'bold',
-						cursor: 'pointer'
-					}}
+            variant={'h6'}
+            sx={{
+              fontSize: '1rem',
+              fontWeight: 'bold',
+              cursor: 'pointer'
+            }}
 				  >{column.title}
 				  </Typography>
 				  <Box>
 					  <Tooltip title={'More option'}>
 						  <ExpandMore
-							sx={{ color: 'text.primary', cursor: 'pointer' }}
-							id="basic-column-dropdown"
-							aria-controls={open ? 'basic-menu-column-dropdown' : undefined}
-							aria-haspopup="true"
-							aria-expanded={open ? 'true' : undefined}
-							onClick={handleClick}
+                sx={{ color: 'text.primary', cursor: 'pointer' }}
+                id="basic-column-dropdown"
+                aria-controls={open ? 'basic-menu-column-dropdown' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
 						  />
 					  </Tooltip>
-					  
+
 					  <Menu
-						id="basic-menu-column-dropdown"
-						aria-labelledby="basic-column-dropdown"
-						anchorEl={anchorEl}
-						open={open}
-						onClose={handleClose}
-						anchorOrigin={{
-							vertical: 'top',
-							horizontal: 'left'
-						}}
-						transformOrigin={{
-							vertical: 'top',
-							horizontal: 'left'
-						}}
+              id="basic-menu-column-dropdown"
+              aria-labelledby="basic-column-dropdown"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
 					  >
 						  <MenuItem onClick={handleClose}>
 							  <ListItemIcon><AddCard fontSize={'small'}/></ListItemIcon>
@@ -137,34 +137,34 @@ function Column({ column }) {
 					  </Menu>
 				  </Box>
 			  </Box>
-			  
-			  
+
+
 			  <ListCards cards={orderedCards}/>
-			  
+
 			  <Box sx={{
 				  height: (theme) => theme.trelloCutom.columnFooterHeight,
 				  p: 1
 			  }}>
 				  {!openNewCardForm ?
-					<Box sx={{
-						height: '100%',
-						display: 'flex',
-						alignItems: 'center',
-						justifyContent: 'space-between'
-					}}>
-						<Button startIcon={<AddCard/>} onClick={toggleNewCardForm}>Add new card</Button>
-						<Tooltip title={'Drag to move'}>
-							<DragHandle sx={{ cursor: 'pointer' }}/>
-						</Tooltip>
-					</Box>
-					:
-					<Box sx={{
-						height: '100%',
-						display: 'flex',
-						alignItems: 'center',
-						gap: 1
-					}}>
-						<TextField
+            <Box sx={{
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between'
+            }}>
+              <Button startIcon={<AddCard/>} onClick={toggleNewCardForm}>Add new card</Button>
+              <Tooltip title={'Drag to move'}>
+                <DragHandle sx={{ cursor: 'pointer' }}/>
+              </Tooltip>
+            </Box>
+            :
+            <Box sx={{
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              gap: 1
+            }}>
+              <TextField
 						  label="Enter card title"
 						  type="text"
 						  size={'small'}
@@ -182,9 +182,9 @@ function Column({ column }) {
 								  '&.Mui-focuced fieldset': { borderColor: 'white' }
 							  }
 						  }}
-						/>
-						<Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-							<Button
+              />
+              <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                <Button
 							  variant={'contained'}
 							  color={'success'}
 							  size={'small'}
@@ -195,19 +195,19 @@ function Column({ column }) {
 								  '&:hover': { bgcolor: (theme) => theme.palette.success.main }
 							  }}
 							  onClick={addNewCard}>Add</Button>
-							<Close
+                <Close
 							  fontSize={'small'}
 							  sx={{ color: (theme) => theme.palette.warning.light, cursor: 'pointer', '&:hover': { color: (theme) => theme.palette.warning.light } }}
 							  onClick={toggleNewCardForm}
-							/>
-						</Box>
-					</Box>
+                />
+              </Box>
+            </Box>
 				  }
-			  
+
 			  </Box>
 		  </Box>
 	  </div>
-	)
+  )
 }
 
 export default Column
